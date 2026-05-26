6819a86493557ba454669d75cc6025d27b2bc5fc
reamplify <126955026+reamplify1@users.noreply.github.com>
2026-05-27 04:53:04 +0500

    feat: configure core application routing and minimal 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./components/home-page/home-page').then(m => m.HomePage),
+    pathMatch: 'full'
+  },
+
+  {
+    path: '404',
+    loadComponent: () => import('./pages/not-found/not-found').then(m => m.NotFound)
+  },
+
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
