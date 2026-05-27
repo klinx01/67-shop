22b326ef4d35dde1897e0660b9edd7d210ef038d
klinx01 <vadimkorotnan@gmail.com>
2026-05-27 17:42:23 +0300

    Revert "Created productService"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,3 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () => import('./components/home-page/home-page').then(m => m.HomePage),
-    pathMatch: 'full'
-  },
-
-  {
-    path: 'cart',
-    loadComponent: () => import('./components/home-page/home-page').then(m => m.HomePage), 
-    canActivate: [authGuard]
-  },
-
-  {
-    path: '404',
-    loadComponent: () => import('./pages/not-found/not-found').then(m => m.NotFound)
-  },
-
-  {
-    path: '**',
-    redirectTo: '404'
-  }
-];
+export const routes: Routes = [];
--- src/app/components/product-filter/product-filter.ts
+++ src/app/components/product-filter/product-filter.ts
@@ -1,4 +1,4 @@
-import { Component, output, OutputEmitterRef } from '@angular/core';
+import { Component, computed, output, OutputEmitterRef, Signal, signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
